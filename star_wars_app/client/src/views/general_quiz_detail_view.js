e622ed3259043338899f031432adb0e27aaba0f9
Scott Osman <codecso@protonmail.ch>
2019-01-21 16:07:29 +0000

    Form working somewhat

--- star_wars_app/client/src/views/general_quiz_detail_view.js
+++ star_wars_app/client/src/views/general_quiz_detail_view.js
@@ -12,30 +12,27 @@ GeneralQuizDetailView.prototype.createGeneralQuizDetail = function(singleQuestio
 
   const form = document.createElement('FORM');
 
-
-
-  this.createInput('radio', 'Answer 1', singleQuestion.answers[0]);
-  this.createInput('radio', 'Answer 2', singleQuestion.answers[1]);
-  this.createInput('radio', 'Answer 3', singleQuestion.answers[2]);
-  this.createInput('radio', 'Answer 4', singleQuestion.answers[3]);
+  this.createInput('radio', 'Answer 1', singleQuestion.answers[0], form);
+  this.createInput('radio', 'Answer 2', singleQuestion.answers[1], form);
+  this.createInput('radio', 'Answer 3', singleQuestion.answers[2], form);
+  this.createInput('radio', 'Answer 4', singleQuestion.answers[3], form);
 
   const submitButton = document.createElement("input");
   submitButton.setAttribute('type',"submit");
   submitButton.setAttribute('value',"Update");
-  this.form.appendChild(submitButton);
+  form.appendChild(submitButton);
 
-  this.container.appendChild(this.form);
+  generalQuizDiv.appendChild(form);
 
   return generalQuizDiv;
-
 }
 
-GeneralQuizDetailView.prototype.createInput = function (type, name, value) {
+GeneralQuizDetailView.prototype.createInput = function (type, name, value, form) {
   const input = document.createElement("input"); //input element, text
   input.setAttribute('type', type);
   input.setAttribute('name', name);
   input.setAttribute('value', value);
-  this.form.appendChild(input);
+  form.appendChild(input);
 }
 
 module.exports = GeneralQuizDetailView;
