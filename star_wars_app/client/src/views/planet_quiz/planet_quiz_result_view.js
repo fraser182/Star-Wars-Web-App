4960811f83686ca967499df6803fe250075d3af2
Scott Osman <codecso@protonmail.ch>
2019-01-22 14:57:39 +0000

    hahaha...

--- star_wars_app/client/src/views/planet_quiz/planet_quiz_result_view.js
+++ star_wars_app/client/src/views/planet_quiz/planet_quiz_result_view.js
@@ -17,8 +17,20 @@ PlanetQuizResultView.prototype.render = function(count){
   const resultDiv = document.createElement('div')
 
   const header = document.createElement('h3');
-  header.textContent = count;
+  header.textContent = 'Result:';
   resultDiv.appendChild(header);
+
+  const message = document.createElement('p');
+  message.textContent = ;
+  resultDiv.appendChild(message);
+
+  const numberCorrect = document.createElement('p');
+  numberCorrect.textContent = ;
+  resultDiv.appendChild(numberCorrect)
+
+  const resultImage = document.createElement('img')
+  resultImage.textContent = ;
+  resultDiv.appendChild(resultImage);
 
   this.container.appendChild(resultDiv);
 }
